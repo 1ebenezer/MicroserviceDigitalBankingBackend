f128ff25da5827c0d1ec8dc76e850fc35a8820a4
1ebenezer <eben4nkusi@gmail.com>
2024-05-30 16:08:09 +0200

    Bill Payment

---
+++ Bill-Payment-service/src/test/java/com/example/Bill_Payment_service/BillPaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Bill_Payment_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillPaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
